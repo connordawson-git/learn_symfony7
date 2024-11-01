94762bfbadb163a94d13f680759b3f4574c09b46
Connor Dawson <c.dawson@digital-velocity.co.uk>
2024-11-01 17:04:20 +0000

    homepage twig created and dummy data of customer name passed to view and echoed

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,14 +2,19 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class MainController
+class MainController extends AbstractController
 {
     #[Route('/')]
     public function homepage(): Response
     {
-        return new Response('<h1>Homepage</h1>');
+        #example data
+        $customerName = "Connor";
+        return $this->render('main/homepage.html.twig', [
+            "customerName" => $customerName
+        ]);
     }
 }
