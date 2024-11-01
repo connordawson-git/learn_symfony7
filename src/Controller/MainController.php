f678ca70825f320b43b36b296167a1f9a4ae1e2e
Connor Dawson <c.dawson@digital-velocity.co.uk>
2024-11-01 16:04:16 +0000

    php fixer included

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,12 +5,11 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class MainController {
-
+class MainController
+{
     #[Route('/')]
     public function homepage(): Response
     {
         return new Response('<h1>Homepage</h1>');
     }
-
 }
