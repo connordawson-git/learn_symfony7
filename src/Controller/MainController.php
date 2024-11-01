74c58e95062792c6a6f835e8f32f66b896eaa4a0
Connor Dawson <c.dawson@digital-velocity.co.uk>
2024-11-01 15:37:07 +0000

    homepage response and route added

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,15 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
 class MainController {
+
+    #[Route('/')]
+    public function homepage(): Response
+    {
+        return new Response('<h1>Homepage</h1>');
+    }
 
 }
